69d6a80b2abe4333ecf16f68a38babf4b8df24c7
Marvin Arnold <marvin@geeky.rocks>
2024-11-15 23:26:12 -0600

    feat: send tg auth token

---
+++ src/generateTelegramHash.ts
@@ -0,0 +1,45 @@
+import * as nodeCrypto from 'crypto';
+
+/**
+ * Function to generate HMAC hash for Telegram authentication
+ * @param {Object} data - User data to be hashed
+ * @returns {string} - Generated HMAC hash
+ */
+export const generateTelegramHash = (data: any, botToken: string) => {
+  // Prepare the data object with required fields
+  const useData = {
+    auth_date: String(data.authDate),
+    first_name: data.firstName,
+    id: String(data.id),
+    last_name: data.lastName,
+    photo_url: data.photoURL,
+    username: data.username,
+  };
+
+  // Filter out undefined or empty values from the data object
+  const filteredUseData = Object.entries(useData).reduce(
+    (acc: { [key: string]: any }, [key, value]) => {
+      if (value) acc[key] = value;
+      return acc;
+    },
+    {} as { [key: string]: any },
+  );
+
+  // Sort the entries and create the data check string
+  const dataCheckArr = Object.entries(filteredUseData)
+    .map(([key, value]) => `${key}=${String(value)}`)
+    .sort((a, b) => a.localeCompare(b))
+    .join('\n');
+
+  // Create SHA-256 hash from the bot token
+  const TELEGRAM_SECRET = nodeCrypto
+    .createHash('sha256')
+    .update(botToken)
+    .digest();
+
+  // Generate HMAC-SHA256 hash from the data check string
+  return nodeCrypto
+    .createHmac('sha256', TELEGRAM_SECRET)
+    .update(dataCheckArr)
+    .digest('hex');
+};
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,59 @@
 import { Telegraf, Markup } from 'telegraf';
 
-import { about } from './commands';
-import { greeting } from './text';
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { development, production } from './core';
+import { generateTelegramHash } from './generateTelegramHash';
+import * as jwt from 'jsonwebtoken';
+
+const TEXT_WELCOME =
+  'Save CROAK and get the chance to win a free Efrog by getting the froglet to the other side of the pond. 🐸 Brought to you by Locker';
+const TEXT_LINK = 'Play now';
+const EFROGR_URL = 'https://efrogr.locker.money';
 
 const BOT_TOKEN = process.env.BOT_TOKEN || '';
 const ENVIRONMENT = process.env.NODE_ENV || '';
 
-const bot = new Telegraf(BOT_TOKEN);
-const welcomeText =
-  'Save CROAK and get the chance to win a free Efrog by getting the froglet to the other side of the pond. 🐸 Brought to you by Locker';
-const linkText = 'Play now';
-const url = 'https://efrogr.locker.money';
+if (!BOT_TOKEN) {
+  console.error('Please add BOT_TOKEN to the .env file');
+  process.exit(1);
+}
 
-bot.start((ctx) =>
+const bot = new Telegraf(BOT_TOKEN);
+
+bot.start((ctx) => {
+  const userData = {
+    authDate: Math.floor(new Date().getTime()),
+    firstName: ctx.update.message.from.first_name,
+    lastName: '',
+    username: ctx.update.message.from.username,
+    id: ctx.update.message.from.id,
+    photoURL: '',
+  };
+
+  // Generate the hash for Telegram authentication
+  const hash = generateTelegramHash(userData, BOT_TOKEN);
+
+  // Create JWT with user data and hash
+  const telegramAuthToken = jwt.sign(
+    {
+      ...userData,
+      hash,
+    },
+    BOT_TOKEN, // Use the bot token to sign the JWT
+    { algorithm: 'HS256' },
+  );
+  console.log('[DEBUG] JWT generated for user', userData);
+
+  // URL-encode the generated JWT for safe usage in a URL
+  const encodedTelegramAuthToken = encodeURIComponent(telegramAuthToken);
+
+  const url = `${EFROGR_URL}/?telegramAuthToken=${encodedTelegramAuthToken}`;
+  console.log('[URL] url generated for user', url);
   ctx.reply(
-    welcomeText,
-    Markup.inlineKeyboard([Markup.button.webApp(linkText, url)]),
-  ),
-);
+    TEXT_WELCOME,
+    Markup.inlineKeyboard([Markup.button.webApp(TEXT_LINK, url)]),
+  );
+});
 
 //prod mode (Vercel)
 export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
